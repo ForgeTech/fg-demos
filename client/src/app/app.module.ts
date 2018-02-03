1c96baa6b9f7933e3a01fc049b6c2eed5821ad26
Florian Neumann <di.neumann.florian@gmail.com>
2018-02-03 19:02:25 +0100

    - Added angular-module holding apollo-client configuration - Installed apollo and graphql packages via npm - Imported and added some basic styling for the application

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { GraphQLModule } from './apollo.config';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GraphQLModule
   ],
   providers: [],
   bootstrap: [AppComponent]
